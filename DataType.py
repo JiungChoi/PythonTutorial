f4ea9f6acd7f0dec8e71b603b7cb88b857183df7
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-04-04 12:36:06 +0900

    Create DataType.py

---
+++ DataType.py
@@ -0,0 +1,26 @@
+
+#덧셈
+print('덧셈')
+print(4 + 7.0)
+
+#뺄셈 
+print('뺄셈')
+print(2 - 4)
+
+#곱셈
+print('곱셈')
+print(5 * 3)
+
+#나머지 
+print('나머지')
+print(7 % 3)
+
+#거듭제곱
+print('거듭제곱')
+print(2.2 ** 3)
+
+#나누기
+print('나누기')
+print(7 / 2)
+print(7.0 / 2)
+print(7 // 2)
