badfcbcc5e5983cde96aa1e692bdce33aea04d1e
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-04-06 12:20:08 +0900

    Create dictionary_basic.py

---
+++ dictionary_basic.py
@@ -0,0 +1,14 @@
+# 사전 (dictionary)
+# key - value pair (키- 값 쌍)
+my_dictionary = {
+    5: 25,
+    2: 4,
+    3: 9,
+    '엄마' : '김자옥',
+    '아빠' : '이석진',
+    '아들' : '이동민'
+}
+print(type(my_dictionary))
+print(my_dictionary[3])
+my_dictionary[4] = 2
+print(my_dictionary)
